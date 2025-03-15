a85f379bfd5724604e4107fdfa2ed9bdeb445c5c
dragos-gruia <dragos.c.gruia@gmail.com>
2025-03-15 12:02:35 +0000

    Fix bug in run all function

--- R_scripts/run_all.R
+++ R_scripts/run_all.R
@@ -4,6 +4,9 @@ directory_path <- "path/to/your/Rscripts"
 # List all files in the directory ending with .R
 r_files <- list.files(path = directory_path, pattern = "\\.R$", full.names = TRUE)
 
+# Remove the current script from the list if its path was determined
+r_files <- r_files[basename(r_files) != "run_all.R"]
+
 # Loop over each file and run it
 for (file in r_files) {
   cat("Sourcing:", file, "\n")
